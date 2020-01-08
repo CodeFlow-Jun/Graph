5f97f6c8ab037366a4c694042f0001f9ccc048e9
dajun <18698853689@163.com>
2020-01-08 21:24:19 +0800

    修改

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,5 @@
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        // TODO: 2020/1/8 添加接口 
+        System.out.println("添加了一个接口");
     }
 }
